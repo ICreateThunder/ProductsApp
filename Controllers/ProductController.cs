dadd4ca7ee2071831a7a71c11e66173423c03c34
ICreateThunder <iamrobshalr@gmail.com>
2023-10-16 13:39:19 +0100

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsApp.Context;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductContext db = new ProductContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            Product product = db.Products.Find(id);
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            Product product = db.Products.Find(id);
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(product).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(product);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            Product product = db.Products.Find(id);
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int ?id, Product product)
+        {
+            try
+            {
+                Product _product = new Product();
+
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                        return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+                    _product = db.Products.Find(id);
+                    
+                    if (_product == null)
+                        return HttpNotFound();
+
+                    db.Products.Remove(_product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(_product);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
