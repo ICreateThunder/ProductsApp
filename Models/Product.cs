10e1e1af42f665a3c4822c1cb5e6f1cddee1d82a
ICreateThunder <iamrobshalr@gmail.com>
2023-10-16 13:47:00 +0100

    [+] Added database annotations for the Product Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,14 @@ namespace ProductsApp.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Product Name")]
+        [StringLength(10)]
         public string ProductName {  get; set; }
+        [Required(ErrorMessage ="Price is required")]
+        [Range(0, 100, ErrorMessage = "Price must be between £0 and £1000")]
         public decimal Price { get; set; }
+        [DisplayName("Description")]
+        [StringLength(50)]
         public string Description {  get; set; }
     }
 }
